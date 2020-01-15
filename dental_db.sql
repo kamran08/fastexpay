8979ceb0cf3ecbecb56239f108501de0146be69f
salman <ahmedkamran265@gmail.com>
2020-01-15 21:15:31 +0600

    ok

--- dental_db.sql
+++ dental_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 14, 2020 at 04:22 PM
+-- Generation Time: Jan 15, 2020 at 04:15 PM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.3.8
 
@@ -72,6 +72,14 @@ CREATE TABLE `images` (
   `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `images`
+--
+
+INSERT INTO `images` (`id`, `reviewId`, `image`, `created_at`, `updated_at`) VALUES
+(1, 1, '/uploads/1579091908269.png', '2020-01-15 18:38:34', '2020-01-15 18:38:34'),
+(2, 1, '/uploads/1579091910302.png', '2020-01-15 18:38:34', '2020-01-15 18:38:34');
+
 -- --------------------------------------------------------
 
 --
@@ -80,14 +88,27 @@ CREATE TABLE `images` (
 
 CREATE TABLE `reviews` (
   `id` int(10) UNSIGNED NOT NULL,
+  `doctorId` int(10) UNSIGNED DEFAULT NULL,
   `name` varchar(191) DEFAULT NULL,
+  `status` varchar(20) NOT NULL DEFAULT 'Pending',
   `date` date DEFAULT NULL,
   `image` varchar(191) DEFAULT NULL,
-  `rate` int(11) DEFAULT NULL,
+  `rate` int(1) DEFAULT 0,
   `description` text DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `reviews`
+--
+
+INSERT INTO `reviews` (`id`, `doctorId`, `name`, `status`, `date`, `image`, `rate`, `description`, `created_at`, `updated_at`) VALUES
+(1, NULL, ' ssdfa', 'Published', '2020-01-10', '/uploads/1579091902253.png', 4, ' sfasdfadfs afd adfs asdf adf', '2020-01-15 18:38:34', '2020-01-15 20:28:31'),
+(2, NULL, 'kamran', 'Declined', '2020-01-16', '/uploads/1579091902253.png', 3, 'sdafa asdf asdf afda fdsfasd', NULL, '2020-01-15 20:26:26'),
+(3, NULL, 'kamran', 'Declined', '2020-01-16', '/uploads/1579091902253.png', 3, 'sdafa asdf asdf afda fdsfasd', NULL, NULL),
+(4, 1, ' dfgsfg', 'Pending', '2020-01-10', '/uploads/1579100841124.png', 4, ' dfg sfg', '2020-01-15 21:08:57', '2020-01-15 21:08:57'),
+(5, 1, ' dfgsfg', 'Pending', '2020-01-10', '/uploads/1579100841124.png', 4, ' dfg sfg', '2020-01-15 21:08:57', '2020-01-15 21:08:57');
 
 -- --------------------------------------------------------
 
@@ -113,8 +134,8 @@ CREATE TABLE `teams` (
 --
 
 INSERT INTO `teams` (`id`, `name`, `email`, `designation`, `image`, `description`, `address`, `phone`, `created_at`, `updated_at`) VALUES
-(1, ' sasdfasdf', ' dfasdfasdf', ' fasdfasfd', '/uploads/1579012743379.png', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).', NULL, NULL, '2020-01-14 20:39:07', '2020-01-14 20:39:07'),
-(2, ' Dr. Shukanto pull', 'aim@gmail.com', 'Dentist', '/uploads/1579014451398.png', '\"Of some description\" is a frequently-encountered but vaguely-defined descriptor used to indicate the level of detail of an object or person without actually describing it. The phrase is used in literature of all sorts. Its use could be seen as an indication that the writer can\'t be bothered to provide a real description or that s/he would rather be meaningfully indefinite, perhaps in an attempt to make the object not described seem enigmatic.\n\nOften, \"of some description\" is used to (not) describe rather ordinary things, e.g.:\n\n\"I have a bowel obstruction of some description.\"\n\"The man left a business card of some description on the table as he departed.\"\n\"I awoke with an insect of some description burrowing into my neck.\"\n\nIt\'s kind of old-fashioned and generally, you don\'t see it all that much. Personally, I like to use it because it sounds silly and it says nothing, really, rather like \"as it were\". It\'s obscure enough that I couldn\'t find a single definition for it on any dictionary or phrase-origin websites. I\'d say it possesses a marked dubiousness of some description. ', NULL, NULL, '2020-01-14 21:07:33', '2020-01-14 21:07:33');
+(1, ' sasdfasdf', ' dfasdfasdf', ' fasdfasfd', '/uploads/1579091902253.png', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).', NULL, NULL, '2020-01-14 20:39:07', '2020-01-14 20:39:07'),
+(2, ' Dr. Shukanto pull', 'aim@gmail.com', 'Dentist', '/uploads/1579091902253.png', '\"Of some description\" is a frequently-encountered but vaguely-defined descriptor used to indicate the level of detail of an object or person without actually describing it. The phrase is used in literature of all sorts. Its use could be seen as an indication that the writer can\'t be bothered to provide a real description or that s/he would rather be meaningfully indefinite, perhaps in an attempt to make the object not described seem enigmatic.\n\nOften, \"of some description\" is used to (not) describe rather ordinary things, e.g.:\n\n\"I have a bowel obstruction of some description.\"\n\"The man left a business card of some description on the table as he departed.\"\n\"I awoke with an insect of some description burrowing into my neck.\"\n\nIt\'s kind of old-fashioned and generally, you don\'t see it all that much. Personally, I like to use it because it sounds silly and it says nothing, really, rather like \"as it were\". It\'s obscure enough that I couldn\'t find a single definition for it on any dictionary or phrase-origin websites. I\'d say it possesses a marked dubiousness of some description. ', NULL, NULL, '2020-01-14 21:07:33', '2020-01-14 21:07:33');
 
 -- --------------------------------------------------------
 
@@ -137,7 +158,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `image`, `name`, `email`, `password`, `created_at`, `updated_at`) VALUES
-(1, '', 'admin', 'admin@gmail.com', '$2y$10$ApX2NLCy2luNW9jRBskdu.ebUaSxSVkB2koLUDwOAdzLX5x4qpJAa', NULL, NULL);
+(1, '/uploads/1579091902253.png', 'admin', 'admin@gmail.com', '$2y$10$ApX2NLCy2luNW9jRBskdu.ebUaSxSVkB2koLUDwOAdzLX5x4qpJAa', NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -200,13 +221,13 @@ ALTER TABLE `appointments`
 -- AUTO_INCREMENT for table `images`
 --
 ALTER TABLE `images`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `reviews`
 --
 ALTER TABLE `reviews`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `teams`
