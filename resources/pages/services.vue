4da500090c75a5ce3562b598345275ba49ef7a78
salman <ahmedkamran265@gmail.com>
2020-02-02 22:15:06 +0600

    new

--- resources/pages/services.vue
+++ resources/pages/services.vue
@@ -3,71 +3,136 @@
         <div class="_content">
         <!--======= Header ======-->
         <div class="_1header">
-          <h2 class="_1header_title">Hygiene</h2>
+          <h2 class="_1header_title">Services</h2>
         </div>
         <!--======= Header end ======-->
 
         <!--======= Form section ======-->
-        <section class="_pages">
+        <section class="_pages _pages_before _ser_listss">
           <div class="_1banner_call">
             <a class="_1banner_call_a" href=""><i class="fas fa-phone"></i> (530) 222-4900</a>
           </div>
 
-          <div class="_pages_main _patientForms_top">
+          <div class="_pages_main ">
             <div class="container">
               <div class="row">
+                <div class="col-12 col-md-5 col-lg-5">
+                  <img src="/uploads/servicess.png" alt="" title="">
+                </div>
                 <div class="col-12 col-md-7 col-lg-7 _patientForms_header_left">
-                  <a class="back _c_default" href=""><i class="fas fa-chevron-left"></i> Back to Service</a>
 
-                  <h2 class="_1title">
+                  <h2 class="_1title _text_left">
                     <span class="_1title_span">If a patient cannot clean his teeth, no dentist can clean them for him.</span>
                   </h2>
 
-                  <p class="_1text">Our team will make you feel right at home when you come in for your check-up. Our team will walk you through each step of your evaluation to make sure you are given every opportunity to ask questions. They will take care of any anxious feelings you may experience, and provide you with the most comfortable care imaginable. You will be asked to share any concerns or changes you may have noticed, such as tooth discoloration, bad breath, sensitivity to hot and cold, or bleeding during brushing or flossing.  The team will then complete a wellness check to make sure they have a comprehensive understanding of your wellness. To do that they will complete:</p>
-                </div>
-                <div class="col-12 col-md-5 col-lg-5">
-                  <div class="_patientForms_header_pic">
-                    <div class="_patientForms_header_imgOne">
-                      <img class="" src="assets/img/pros.png" alt="" title="">
-                    </div>
-                    <div class="_patientForms_header_imgTwo">
-                      <img class="" src="assets/img/1.jpg" alt="" title="">
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="_details_list">
-            <div class="container">
-              <div class="row">
-                <div class="col-12 col-md-6 col-lg-6">
-                  <ul class="_details_list_ul">
-                    <li>Review of your health history (medication use, changes in systemic health, etc.)</li>
-
-                    <li>Gum disease assessment</li>
-
-                    <li>Bone health assessment</li>
-
-                    <li>Check of existing restorations</li>
-                  </ul>
-                </div>
-
-                <div class="col-12 col-md-6 col-lg-6">
-                  <ul class="_details_list_ul">
-                    <li>Oral cancer screening</li>
-
-                    <li>Home care routine review (brushing and flossing)</li>
-
-                    <li>X-rays if necessary•Address any other concerns you may have</li>
-                  </ul>
+                  <p class="_1text _text_left">Our team will make you feel right at home when you come in for your check-up. Our team will walk you through each step of your evaluation to make sure you are given every opportunity to ask questions. They will take care of any anxious feelings you may experience, and provide you with the most comfortable care imaginable. You will be asked to share any concerns or changes you may have noticed, such as tooth discoloration, bad breath, sensitivity to hot and cold, or bleeding during brushing or flossing.  The team will then complete a wellness check to make sure they have a comprehensive understanding of your wellness. To do that they will complete:</p>
                 </div>
               </div>
             </div>
           </div>
         </section>
         <!--======= Form section end ======-->
+
+        <!--======= Lists ===========-->
+        <section class="_ser_listss">
+            <div class="container">
+                <div class="_ser_section_cards">
+                    <div class="row">
+                        <div class="col-6 col-md-4 col-lg-4">
+                          <nuxt-link to="/hygiene">
+                            <div class="_ser_card _1box_shadow _mar_b30">
+                                <div class="_ser_card_pic">
+                                  <img src="/images/tooth.png" alt="" title="" class="_ser_card_img">
+                                  <img src="/images/tooth.png" alt="" title="" class="_ser_card_img _ser_card_img2">
+                                </div>
+                                <div class="_flex_space">
+                                    <h4 class="_ser_card_title">Hygiene</h4></div>
+                                <Icon type="ios-arrow-forward" />
+                            </div>
+                          </nuxt-link>
+                        </div>
+                        <div class="col-6 col-md-4 col-lg-4">
+                            <div class="_ser_card _1box_shadow _mar_b30">
+                                <div class="_ser_card_pic"><img src="/images/tooth2.png" alt="" title="" class="_ser_card_img"></div>
+                                <div class="_flex_space">
+                                    <h4 class="_ser_card_title">Composite &amp; Amalgam Fillings</h4></div>
+                            </div>
+                        </div>
+                        <div class="col-6 col-md-4 col-lg-4">
+                            <div class="_ser_card _1box_shadow _mar_b30">
+                                <div class="_ser_card_pic"><img src="/images/tooth3.png" alt="" title="" class="_ser_card_img"></div>
+                                <div class="_flex_space">
+                                    <h4 class="_ser_card_title">Full &amp; Partial Dentures</h4></div>
+                            </div>
+                        </div>
+                        <div class="col-6 col-md-4 col-lg-4">
+                            <div class="_ser_card _1box_shadow _mar_b30">
+                                <div class="_ser_card_pic"><img src="/images/tooth4.png" alt="" title="" class="_ser_card_img"></div>
+                                <div class="_flex_space">
+                                    <h4 class="_ser_card_title">Porcelain Crowns &amp; Bridges</h4></div>
+                            </div>
+                        </div>
+                        <div class="col-6 col-md-4 col-lg-4">
+                            <div class="_ser_card _1box_shadow _mar_b30">
+                                <div class="_ser_card_pic"><img src="/images/dentalCare.png" alt="" title="" class="_ser_card_img"></div>
+                                <div class="_flex_space">
+                                    <h4 class="_ser_card_title">Extractions</h4></div>
+                            </div>
+                        </div>
+                        <div class="col-6 col-md-4 col-lg-4">
+                            <div class="_ser_card _1box_shadow _mar_b30">
+                                <div class="_ser_card_pic"><img src="/images/impant.png" alt="" title="" class="_ser_card_img"></div>
+                                <div class="_flex_space">
+                                    <h4 class="_ser_card_title">Restoration of Implants</h4></div>
+                            </div>
+                        </div>
+                        <div class="col-6 col-md-4 col-lg-4">
+                            <div class="_ser_card _1box_shadow _mar_b30">
+                                <div class="_ser_card_pic"><img src="/images/xRay.png" alt="" title="" class="_ser_card_img"></div>
+                                <div class="_flex_space">
+                                    <h4 class="_ser_card_title">Digital X-Rays</h4></div>
+                            </div>
+                        </div>
+                        <div class="col-6 col-md-4 col-lg-4">
+                            <div class="_ser_card _1box_shadow _mar_b30">
+                                <div class="_ser_card_pic"><img src="/images/tooth5.png" alt="" title="" class="_ser_card_img"></div>
+                                <div class="_flex_space">
+                                    <h4 class="_ser_card_title">Periodontal Disease &amp; Treatments</h4></div>
+                            </div>
+                        </div>
+                        <div class="col-6 col-md-4 col-lg-4">
+                            <div class="_ser_card _1box_shadow _mar_b30">
+                                <div class="_ser_card_pic"><img src="/images/tooth6.png" alt="" title="" class="_ser_card_img"></div>
+                                <div class="_flex_space">
+                                    <h4 class="_ser_card_title">Teeth Whitening</h4></div>
+                            </div>
+                        </div>
+                        <div class="col-6 col-md-4 col-lg-4">
+                            <div class="_ser_card _1box_shadow _mar_b30">
+                                <div class="_ser_card_pic"><img src="/images/drill.png" alt="" title="" class="_ser_card_img"></div>
+                                <div class="_flex_space">
+                                    <h4 class="_ser_card_title">Porcelain Veneers</h4></div>
+                            </div>
+                        </div>
+                        <div class="col-6 col-md-4 col-lg-4">
+                            <div class="_ser_card _1box_shadow _mar_b30">
+                                <div class="_ser_card_pic"><img src="/images/smiling.png" alt="" title="" class="_ser_card_img"></div>
+                                <div class="_flex_space">
+                                    <h4 class="_ser_card_title">Oral Surgery</h4></div>
+                            </div>
+                        </div>
+                        <div class="col-6 col-md-4 col-lg-4">
+                            <div class="_ser_card _1box_shadow _mar_b30">
+                                <div class="_ser_card_pic"><img src="/images/mouthwash.png" alt="" title="" class="_ser_card_img"></div>
+                                <div class="_flex_space">
+                                    <h4 class="_ser_card_title">Sealants &amp; Fluoride</h4></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--======= Lists ===========-->
 
         <!--======= Schedule section ======-->
         <section class="_schedule">
