b7181147782fd398ffb5e9e81f72583d9f7d5f42
Kollol <dazzling.cloudlet@gmail.com>
2020-04-15 16:08:40 +0600

    werwe

--- start/firebase.js
+++ start/firebase.js
@@ -2,9 +2,9 @@ var admin = require("firebase-admin");
 
 var serviceAccount = require("../app/Controllers/fastexpay-b933a-firebase-adminsdk-sxa7w-ae720059a2.json");
 if (!admin.apps.length) {
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://fastexpay-b933a.firebaseio.com"
-});
+    var FbAdmin = admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+        databaseURL: "https://fastexpay-b933a.firebaseio.com"
+    });
 }
 module.exports.admin = FbAdmin
