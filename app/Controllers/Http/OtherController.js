ae9c816bf1eb9a21d15b87795a1b18fd4f5a39b0
itskamranweb <ahmedkamran265@gmail.com>
2020-04-18 22:57:01 +0600

    updated

--- app/Controllers/Http/OtherController.js
+++ app/Controllers/Http/OtherController.js
@@ -1,20 +1,40 @@
 'use strict'
 const Country = use('App/Models/Country');
+const Division = use('App/Models/Division');
+const SubDivision = use('App/Models/SubDivision');
+const State = use('App/Models/State');
 class OtherController {
 
 
     // 
     async getCountry ({ params, request, response }) {
-        return await Country.all()
+
+        let countries =  await Country.all()
+        return response.status(401).json({
+            countries: countries,
+            success: true,
+        })
     }
     async getDivisions ({ params, request, response }) {
-        return await Country.query().where('country_id', params.id).fetch()
+        let divisions = await Division.query().where('country_id', params.id).fetch()
+        return response.status(401).json({
+            divisions: divisions,
+            success: true,
+        })
     }
     async getSubDivisions ({ params, request, response }) {
-        return await Country.query().where('division_id', params.id).fetch()
+        let subDiv = await SubDivision.query().where('division_id', params.id).fetch()
+        return response.status(401).json({
+            sub_divisions: subDiv,
+            success: true,
+        })
     }
     async getState ({ params, request, response }) {
-        return await Country.query().where('sub_division_id', params.id).fetch()
+        let states = await State.query().where('sub_division_id', params.id).fetch()
+        return response.status(401).json({
+            states: states,
+            success: true,
+        })
     }
 }
 
