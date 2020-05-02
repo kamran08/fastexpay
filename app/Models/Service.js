4572fa6a92920c8eba7e6c020e6a0d0c36d3923b
itskamranweb <ahmedkamran265@gmail.com>
2020-05-03 04:50:23 +0600

    okkd'

--- app/Models/Service.js
+++ app/Models/Service.js
@@ -29,7 +29,7 @@ class Service extends Model {
         return this.hasMany('App/Models/ServiceImage', 'id','service_id')
     }
     avgRating() {
-        return this.hasOne('App/Models/Review', 'id', 'service_id').select('service_id', Database.raw('(cast(AVG(rate) as decimal(10,2))) AS averageRating'), Database.raw('(cast(sum(rate) as decimal(10,2))) AS totalRating'))
+        return this.hasOne('App/Models/Review', 'id', 'service_id').select('service_id', Database.raw('(cast(AVG(rate) as decimal(10,2))) AS averageRating'))
     }
     // avgRating2() {
     //     return this.hasOne('App/Models/Review', 'id', 'reviewFor').select('reviewFor', Database.raw('(cast(AVG(rate) as decimal(10,2))) AS averageRating'), Database.raw('(cast(sum(rate) as decimal(10,2))) AS totalRating'), Database.raw('(cast(AVG(healthyIndex) as decimal(10,2))) AS averageHealthy'), Database.raw('(cast(sum(healthyIndex) as decimal(10,2))) AS totalHealthy'), Database.raw('(cast(sum(harmfulIndex) as decimal(10,2))) AS totalHarmful'), Database.raw('(cast(AVG(harmfulIndex) as decimal(10,2))) AS averageHarmful')).where('review_type', 'school')
