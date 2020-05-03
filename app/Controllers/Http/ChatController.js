f492bad874192e583af19cdd8db18b8a46a0f8a3
Kollol Chakraborty <dazzling.cloudlet@gmail.com>
2020-05-03 23:13:18 +0600

    werwer

--- app/Controllers/Http/ChatController.js
+++ app/Controllers/Http/ChatController.js
@@ -210,6 +210,29 @@ class ChatController {
         io.on('connection', function (socket) {
             console.log('a user connected');
 
+            console.log('socket id', socket.id)
+
+            console.log('query', socket.request._query.conversation_id)
+            // console.log('query', socket._query.orderId)
+            // sender
+            socket.on(`conversation_id_${socket.request._query.conversation_id}`, (data) => {
+
+                // reciver
+                io.emit(`conversation_id_from_server_${socket.request._query.conversation_id}`, data)
+            })
+
+            socket.on(`chat_typing_${socket.request._query.conversation_id}`, (data) => {
+
+                // reciver
+                io.emit(`chat_typing_from_server_${socket.request._query.conversation_id}`, data)
+            })
+
+            socket.on('disconnect', function () {
+                console.log(`${socket.request._query.conversation_id} no driver has been disconnected!`)
+
+                io.emit(`driver_${socket.request._query.conversation_id}_disconnected`, `${socket.request._query.conversation_id} no driver has been disconnected!`)
+            });
+
             // disconnect
             socket.on('disconnect', function () {
                 console.log('user disconnected');
